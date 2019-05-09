18519f967fcdc63c2a9d9014888cb96ff397def3
lml <soalin228@163.com>
2019-05-09 10:56:52 +0800

    js使用立即函数来进行模块化

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,16 +1,18 @@
-var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+!function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-})
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  })
+}.call()
--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -1,8 +1,10 @@
-// 控制导航栏黏在顶部
-window.addEventListener('scroll', function () {
-  if (window.scrollY > 0) {
-    topNavBar.classList.add('sticky')
-  } else {
-    topNavBar.classList.remove('sticky')
-  }
-})
+!function () {
+  // 控制导航栏黏在顶部
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 0) {
+      topNavBar.classList.add('sticky')
+    } else {
+      topNavBar.classList.remove('sticky')
+    }
+  })
+}.call()
